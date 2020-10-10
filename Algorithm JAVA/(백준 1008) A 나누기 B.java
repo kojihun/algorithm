2a65dfa8df29afdc5dfc7d8bb6c1d665246e1212
kojihun <38236367+kojihun@users.noreply.github.com>
2020-10-10 11:47:44 +0900

    (백준 1008) A/B
    <문제>
    두 정수 A와 B를 입력받은 다음, A/B를 출력하는 프로그램을 작성하시오.
    
    <해결방법>
    1. Scanner를 import한 뒤, 키보드로 부터 실수를 입력받아 출력문에서 나눗셈을 연산하였다.

---
+++ Algorithm JAVA/(백준 1008) A 나누기 B.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * @author Kojihun
+ * (백준 1008) A/B
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        double A = sc.nextFloat();
+        double B = sc.nextFloat();
+        
+        System.out.print(A/B);
+    }    
+}
