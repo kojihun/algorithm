7faa5f005eb41e0f21069e94ec5e0b688a4a283d
kojihun <38236367+kojihun@users.noreply.github.com>
2020-10-10 11:36:48 +0900

    (백준 10171) 고양이
    <문제>
    아래 예제와 같이 고양이를 출력하시오.
    \    /\
     )  ( ')
    (  /  )
     \(__)|
    
    <해결방법>
    1. 출력문을 사용하여, 고양이를 출력하였고, \를 표현하기 위해 해당 문자열을 두번 입력하였다.

---
+++ Algorithm JAVA/(백준 10171) 고양이.java
@@ -0,0 +1,13 @@
+/**
+ * @author Kojihun
+ * (백준 10171) 고양이
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }    
+}
