5e49ce24d3c54710e058ffa6cd4a4ad3b45577d1
kojihun <38236367+kojihun@users.noreply.github.com>
2020-10-10 11:40:36 +0900

    (백준 10718) We love kriii
    <문제>
    ACM-ICPC 인터넷 예선, Regional, 그리고 World Finals까지 이미 2회씩 진출해버린 kriii는 미련을 버리지 못하고 왠지 모르게 올 해에도 파주 World Finals 준비 캠프에 참여했다.
    대회를 뜰 줄 모르는 지박령 kriii를 위해서 격려의 문구를 출력해주자.
    
    <출력>
    강한친구 대한육군
    강한친구 대한육군
    
    <해결방법>
    1. 출력문을 이용하여, 해당 문자열을 두번 출력하였다.

---
+++ Algorithm JAVA/(백준 10718) We love kriii.java
@@ -0,0 +1,11 @@
+/**
+ * @author Kojihun
+ * (백준 10718) We love kriii
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("강한친구 대한육군");
+        System.out.println("강한친구 대한육군");
+    }    
+}
