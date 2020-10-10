149bbf7b891cb611520d75bcd6a65cd3a7c7c854
kojihun <38236367+kojihun@users.noreply.github.com>
2020-10-10 11:30:01 +0900

    (백준 1000) A+B
    <문제>
    두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
    
    <해결방법>
    1. Scanner를 import하여, 키보드로 부터 정수를 입력받아 출력문에서 더하기 연산을 이용하여 출력하였다.

---
+++ Algorithm JAVA/(백준 1000) A+B.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+/**
+ * @author Kojihun
+ * (백준 2557) Hello World!
+ */
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int A = sc.nextInt();
+        int B = sc.nextInt();
+        
+        System.out.print(A+B);
+    }    
+}
