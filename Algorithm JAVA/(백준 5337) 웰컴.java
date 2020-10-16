f8c87c82d5d89e005e459e89252f5a2a9b5a71fa
kojihun <38236367+kojihun@users.noreply.github.com>
2020-10-16 10:38:33 +0900

    (백준 5337) 웰컴
    (백준 5337) 웰컴

---
+++ Algorithm JAVA/(백준 5337) 웰컴.java
@@ -0,0 +1,12 @@
+/**
+ * @author Kojihun
+ * (백준 5337) 웰컴
+ */
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println(".  .   .");
+        System.out.println("|  | _ | _. _ ._ _  _");
+        System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
