5360e91611cde02b4b2f9d97c84d7247ffff9f44
kojihun <38236367+kojihun@users.noreply.github.com>
2020-10-16 10:38:45 +0900

    (백준 10926) 물음표물음표느낌표
    (백준 10926) 물음표물음표느낌표

---
+++ Algorithm JAVA/(백준 10926) 물음표물음표느낌표.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * @author Kojihun
+ * (백준 10926) ??!
+ */
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println(input + "??!");
+    }
+}
