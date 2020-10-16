574c912a213c1aa425411ecc3e446d836ec1d605
kojihun <38236367+kojihun@users.noreply.github.com>
2020-10-16 10:25:16 +0900

    (백준 5338) 마이크로소프트 로고
    (백준 5338) 마이크로소프트 로고

---
+++ Algorithm JAVA/(백준 5338) 마이크로소프트 로고.java
@@ -0,0 +1,14 @@
+/**
+ * @author Kojihun
+ * (백준 5338) 마이크로소프트 로고
+ */
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("       _.-;;-._");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-;;-._|");
+        System.out.println("'-..-'|   ||   |");
+        System.out.println("'-..-'|_.-''-._|");
+    }
+}
