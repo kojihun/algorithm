4c5ec20b4a255ab50710dcdf782a964b17f09080
kojihun <38236367+kojihun@users.noreply.github.com>
2020-11-02 18:20:26 +0900

    (백준 1026) 보물
    <문제>
    옛날 옛적에 수학이 항상 큰 골칫거리였던 나라가 있었다. 이 나라의 국왕 김지민은 다음과 같은 문제를 내고 큰 상금을 걸었다.
    
    길이가 N인 정수 배열 A와 B가 있다. 다음과 같이 함수 S를 정의하자.
    
    S = A[0]*B[0] + ... + A[N-1]*B[N-1]
    
    S의 값을 가장 작게 만들기 위해 A의 수를 재배열하자. 단, B에 있는 수는 재배열하면 안 된다.
    
    S의 최솟값을 출력하는 프로그램을 작성하시오.
    
    <해결방법>
    1. 최솟값을 구해야하기때문에 각각의 배열을 정렬한 뒤, 가장 큰 값과 가장 작은 값을 곱하고 곱하였다.
    2. 곱한 값들을 각각 더해 화면에 출력하였다.

---
+++ Algorithm JAVA/(백준 1026) 보물.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author Kojihun 
+ * (백준 1026) 보물
+ */
+public class Main {
+    public static void main(String[] args) throws IOException{
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int S = 0;
+        
+        int[] A = new int[num];
+        int[] B = new int[num];
+        
+        for(int i = 0; i < A.length; i++){
+            A[i] = sc.nextInt();
+        }
+        
+        for(int i = 0; i < B.length; i++){
+            B[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        for(int i = 0; i < num; i++){
+            S = S + (A[i] * B[num - 1 - i]);
+        }
+        
+        System.out.println(S);
+    }
+}
