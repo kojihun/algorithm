5eba56a1d7349a9faaf15772c44b3dce2d08734f
kojihun <38236367+kojihun@users.noreply.github.com>
2020-11-10 23:38:18 +0900

    (백준 11942) 고려대는 사랑입니다.

---
+++ Algorithm JAVA/(백준 11942) 고려대는 사랑입니다.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * @author Kojihun 
+ * (백준 11942) 고려대는 사랑입니다.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException{
+        System.out.println("고려대학교");
+    }
+}
