914a4e2b4cefa52ddb1422f2213b0075ad589bda
kojihun <38236367+kojihun@users.noreply.github.com>
2020-12-16 19:10:13 +0900

    (코드업) [기초-출력] 출력하기02(설명)
    (코드업) [기초-출력] 출력하기02(설명)

---
+++ (코드업) [기초-출력] 출력하기02(설명).java
@@ -0,0 +1,12 @@
+﻿import java.io.*;
+import java.util.*;
+
+/**
+ * @author Kojihun (CodeUp) 출력하기
+ */
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.print("Hello World");
+    }
+}
