8ecca789be01730a735122cd8187ba03e2a83171
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-02-08 11:41:04 +0900

    [level 1] Title: 모의고사, Time: 2.83 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,43 +1,39 @@
 import java.util.*;
+
 /*
-문제IDEA
-1번: 1,2,3,4,5
-2번: 2,1,2,3,2,4,2,5
-3번: 3,3,1,1,2,2,4,4,5,5
+1. 1, 2, 3, 4, 5
+2. 2, 1, 2, 3, 2, 4, 2, 5
+3. 3, 3, 1, 1, 2, 2, 4, 4, 5, 5
 */
 class Solution {
     public int[] solution(int[] answers) {
-        int[] person1 = {1, 2, 3, 4, 5};
-        int[] person2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] person3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
+        int[] player1 = {1, 2, 3, 4, 5};
+        int[] player2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] player3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         int[] scores = new int[3];
         
         for(int i = 0; i < answers.length; i++) {
-            if(answers[i] == person1[i % person1.length]) {
+            if(answers[i] == player1[i % player1.length]) {
                 scores[0]++;
             }
-            if(answers[i] == person2[i % person2.length]) {
+            
+            if(answers[i] == player2[i % player2.length]) {
                 scores[1]++;
             }
-            if(answers[i] == person3[i % person3.length]) {
+            
+            if(answers[i] == player3[i % player3.length]) {
                 scores[2]++;
             }
         }
         
         int maxValue = Math.max(scores[0], Math.max(scores[1], scores[2]));
-        
-        List<Integer> list = new ArrayList<>();
+        List<Integer> answer = new ArrayList<>();
         for(int i = 0; i < scores.length; i++) {
             if(scores[i] == maxValue) {
-                list.add(i + 1);
+                answer.add(i + 1);
             }
         }
         
-        int[] answer = new int[list.size()];
-        for(int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
-        
-        return answer;
+        return answer.stream().mapToInt(Integer::intValue).toArray();
     }
 }
