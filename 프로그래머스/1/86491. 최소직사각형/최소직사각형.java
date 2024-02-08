b393760539d491f40eddea25d1e4a3dcad410e91
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-02-08 11:21:18 +0900

    [level 1] Title: 최소직사각형, Time: 5.03 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.java
@@ -1,23 +1,33 @@
 import java.util.*;
 
-/*
-풀이IDEA
-1. 문제의 핵심 요지는 가로와 세로를 곱하여 가장 작은 직사각형을 만드는 것
-2. 각 행에 대해 정렬을 통해 가로길이는 짧은 것들만 모이게 하고, 세로길이는 긴 것들만 모이게 하자
-3. 두 변의 곱을 구하면 최소 직사각형을 구할 수 있다.
-*/
 class Solution {
     public int solution(int[][] sizes) {
-        int maxWidth = 0;
-        int maxHeight = 0;
+        int minValue = Integer.MIN_VALUE;
+        int maxValue = Integer.MIN_VALUE;
         
-        for(int[] size : sizes) {
-            Arrays.sort(size);
-            
-            maxWidth = Math.max(maxWidth, size[0]);
-            maxHeight = Math.max(maxHeight, size[1]);
+        for(int i = 0; i < sizes.length; i++) {
+            Arrays.sort(sizes[i]);        
         }
         
-        return maxWidth * maxHeight;
+        for(int i = 0; i < sizes.length; i++) {
+            if(sizes[i][0] > minValue) {
+                minValue = sizes[i][0];
+            }
+            
+            if(sizes[i][1] > maxValue) {
+                maxValue = sizes[i][1];
+            }
+        }
+        
+        return minValue * maxValue;
+    }
+    
+    public static void print(int[][] sizes) {
+        for(int i = 0; i < sizes.length; i++) {
+            for(int j = 0; j < sizes[0].length; j++) {
+                System.out.print(sizes[i][j] + " ");
+            }
+            System.out.println();
+        }
     }
 }
