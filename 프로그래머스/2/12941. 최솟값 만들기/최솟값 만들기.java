636613733846e81a3ddcfac29261bc0d0752eee7
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-02-14 00:09:24 +0900

    [level 2] Title: 최솟값 만들기, Time: 2.19 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
+++ 프로그래머스/2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -1,32 +1,19 @@
-import java.util.*;
+import java.util.Arrays;
 
-class Solution
-{
-    public int solution(int[] A, int[] B)
-    {
+class Solution {
+    public int solution(int[] A, int[] B) {
+        // 1. A배열은 오름차순, B배열은 내림차순으로 정렬한다.
         Arrays.sort(A);
-        Arrays.sort(B);   
-        reverseSort(B);
+        Arrays.sort(B);
         
+        // 2. 배열을 순회하며 곱한 값을 answer에 더한다.
         int answer = 0;
+        int index = B.length - 1;
         for(int i = 0; i < A.length; i++) {
-            answer += A[i] * B[i];
+            answer += A[i] * B[index--];
         }
-
+        
+        // 3. 결과 값 리턴
         return answer;
-    }
-    
-    public static void reverseSort(int[] B) {
-        int left = 0;
-        int right = B.length - 1;
-
-        while (left < right) {
-            int temp = B[left];
-            B[left] = B[right];
-            B[right] = temp;
-
-            left++;
-            right--;
-        }
     }
 }
