572fa4ef1ed9126ea3a2f515ebe44266fb69487c
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-02-14 00:29:11 +0900

    [level 2] Title: 올바른 괄호, Time: 19.70 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -2,19 +2,23 @@ import java.util.Stack;
 
 class Solution {
     boolean solution(String s) {
-        Stack<Character> stack = new Stack<>(); 
-        char[] brackets = s.toCharArray();
-
-        for (char bracket : brackets) {
-            if (bracket == '(') {
-                stack.push(bracket);
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push(c);
             } else {
-                if (stack.isEmpty() || stack.pop() != '(') {
+                // ')'일 때 스택에서 '('를 제거하며 검사
+                if (!stack.isEmpty() && stack.peek() == '(') {
+                    stack.pop();
+                } else {
+                    // 스택이 비어있거나 pop할 '('이 없는 경우 올바른 괄호 문자열이 아니므로 false 반환
                     return false;
                 }
             }
         }
-
+        
+        // 모든 문자를 확인한 후 스택이 비어있다면 올바른 괄호 문자열이므로 true 반환
         return stack.isEmpty();
     }
 }
