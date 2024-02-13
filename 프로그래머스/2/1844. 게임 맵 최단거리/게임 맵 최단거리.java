3c49b6470a00ea14201385abd9163a5a58677888
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-02-13 12:00:34 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 3.15 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,45 +1,34 @@
 import java.util.*;
 
 class Solution {
-    public static int[] xways = {1, 0, -1, 0};
-    public static int[] yways = {0, 1, 0, -1};
-    
     public int solution(int[][] maps) {
-        int answer = BFS(maps);
-        return answer;
-    }
-    
-    public int BFS(int[][] maps) {
         int n = maps.length;
         int m = maps[0].length;
         
-        Queue<int[]> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[n][m];
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, 1, 0, -1};
         
-        int[] initPos = {0, 0, 1};
-        queue.offer(initPos);
-        visited[0][0] = true;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{0, 0, 1});
+        maps[0][0] = 0;
         
         while(!queue.isEmpty()) {
             int[] current = queue.poll();
             int x = current[0];
             int y = current[1];
-            int count = current[2];
+            int dist = current[2];
             
-            if(x == n -1 && y == m -1) {
-                return count;
+            if(x == n - 1 && y == m - 1) {
+                return dist;
             }
             
             for(int i = 0; i < 4; i++) {
-                int nx = x + xways[i];
-                int ny = y + yways[i];
+                int nx = x + dx[i];
+                int ny = y + dy[i];
                 
-                if(nx >= 0 && ny >= 0 && nx < n && ny < m) {
-                    if(maps[nx][ny] == 1 && !visited[nx][ny]) {
-                        int[] newPos = {nx, ny, count + 1};
-                        queue.offer(newPos);
-                        visited[nx][ny] = true;
-                    }
+                if(nx >= 0 && ny >= 0 && nx < n && ny < m && maps[nx][ny] == 1) {
+                    queue.offer(new int[]{nx, ny, dist + 1});
+                    maps[nx][ny] = 0;
                 }
             }
         }
