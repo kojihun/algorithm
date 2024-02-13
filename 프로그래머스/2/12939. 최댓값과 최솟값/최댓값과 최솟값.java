da6822a622cd893be79fbce9fbc162a073930e52
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-02-13 23:20:07 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 7.56 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.java
@@ -1,21 +1,25 @@
 class Solution {
     public String solution(String s) {
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        
+        // 1. 문자열을 공백 기준으로 배열에 담는다.
         String[] input = s.split(" ");
+        
+        // 2. 최댓값과 최솟값을 저장할 변수를 선언한다.
+        int maxValue = Integer.MIN_VALUE;
+        int minValue = Integer.MAX_VALUE;
+        
+        // 3. 반복문을 순회하며, 최댓값과 최솟값을 찾는다.
         for(int i = 0; i < input.length; i++) {
-            int currentNumber = Integer.parseInt(input[i]);
-            if(currentNumber > max) {
-                max = currentNumber;
+            int curNumber = Integer.parseInt(input[i]);
+            if(maxValue < curNumber) {
+                maxValue = curNumber;
             }
             
-            if(currentNumber < min) {
-                min = currentNumber;
+            if(minValue > curNumber) {
+                minValue = curNumber;
             }
         }
         
-        String answer = min + " " + max;
-        return answer;
+        // 4. 공백을 기준으로 최솟값과 최댓값을 나란히 리턴한다.
+        return minValue + " " + maxValue;
     }
 }
