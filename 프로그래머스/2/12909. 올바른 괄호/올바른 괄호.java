730ae6b9f7866b7f9d4f8c10c2dccfe4715852ed
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-02-27 14:48:33 +0900

    [level 2] Title: 올바른 괄호, Time: 18.25 ms, Memory: 52.7 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -1,24 +1,32 @@
-import java.util.Stack;
+import java.util.*;
 
 class Solution {
     boolean solution(String s) {
-        Stack<Character> stack = new Stack<>();
+        boolean answer = true;
         
-        for (char c : s.toCharArray()) {
-            if (c == '(') {
+        // 1. 문자 단위로 쪼개기
+        char[] input = s.toCharArray();
+        
+        // 2. 열린 괄호일 경우 스택에 추가
+        Stack<Character> stack = new Stack<>();
+        for(char c : input) {
+            if(c == '(') {
                 stack.push(c);
-            } else {
-                // ')'일 때 스택에서 '('를 제거하며 검사
-                if (!stack.isEmpty() && stack.peek() == '(') {
+            }else {
+                if(!stack.empty() && stack.peek() == '(') {
                     stack.pop();
-                } else {
-                    // 스택이 비어있거나 pop할 '('이 없는 경우 올바른 괄호 문자열이 아니므로 false 반환
-                    return false;
+                }else {
+                    answer = false;
+                    break;
                 }
             }
         }
         
-        // 모든 문자를 확인한 후 스택이 비어있다면 올바른 괄호 문자열이므로 true 반환
-        return stack.isEmpty();
+        // 3. 결과 값 반환
+        if(!stack.empty()) {
+            answer = false;
+        }
+        
+        return answer;
     }
 }
