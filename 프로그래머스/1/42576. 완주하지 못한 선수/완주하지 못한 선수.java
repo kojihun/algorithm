c0438ccfcd0b14d7b48ba26f2824143f0e55a901
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-03-20 11:46:58 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 92.67 ms, Memory: 94.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -2,27 +2,25 @@ import java.util.*;
 
 class Solution {
     public String solution(String[] participant, String[] completion) {
-        // 참가자 이름과 그 이름의 등장 횟수를 저장하는 HashMap을 생성합니다.
-        HashMap<String, Integer> map = new HashMap<>();
-        
-        // 참가자 배열에 등장하는 이름들과 그 등장 횟수를 저장합니다.
-        for (String p : participant) {
-            map.put(p, map.getOrDefault(p, 0) + 1);
+        Map<String, Integer> map = new HashMap<>();
+        for(int i = 0; i < participant.length; i++) {
+            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
         }
         
-        // 완주자 배열에 등장하는 이름들의 등장 횟수를 차감합니다.
-        for (String c : completion) {
-            map.put(c, map.get(c) - 1);
+        for(int i = 0; i < completion.length; i++) {
+            map.put(completion[i], map.get(completion[i]) - 1);
         }
-        
-        // 등장 횟수가 0보다 큰 이름이 완주하지 못한 선수입니다.
-        for (String key : map.keySet()) {
-            if (map.get(key) > 0) {
-                return key;
+
+        String answer = "";
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            String key = entry.getKey();
+            int value = entry.getValue();
+            
+            if(value == 1) {
+                answer = key;
             }
         }
         
-        // 여기까지 도달한다면 완주하지 못한 선수는 항상 존재하므로, 이 코드는 실행되지 않습니다.
-        return null;
+        return answer;
     }
 }
