898da03b8de00cc761d4f35e10470a0a149cc810
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-03-21 14:38:10 +0900

    [level 1] Title: 체육복, Time: 0.34 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?itm_content=course14743) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 73.6 MB, 시간: 0.02 ms
+메모리: 78.2 MB, 시간: 0.34 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 2월 4일 12:50:24
+2024년 03월 21일 14:38:08
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -2,37 +2,28 @@ import java.util.*;
 
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int[] clothes = new int[n];
-        Arrays.fill(clothes, 1); // 모든 학생은 체육복 1벌을 가짐
+        Arrays.sort(reserve);
+        Arrays.sort(lost);
         
-        // 체육복을 잃어버린 학생 처리
-        for (int l : lost) {
-            clothes[l - 1]--;
-        }
-        
-        // 여벌의 체육복을 가진 학생 처리
-        for (int r : reserve) {
-            clothes[r - 1]++;
-        }
-        
-        // 체육복 빌려주기
-        for (int i = 0; i < clothes.length; i++) {
-            if (clothes[i] == 0) {
-                if (i - 1 >= 0 && clothes[i - 1] == 2) { // 왼쪽 학생이 여분이 있는 경우
-                    clothes[i]++;
-                    clothes[i - 1]--;
-                } else if (i + 1 < clothes.length && clothes[i + 1] == 2) { // 오른쪽 학생이 여분이 있는 경우
-                    clothes[i]++;
-                    clothes[i + 1]--;
+        int answer = n - lost.length;
+        for(int i = 0; i < lost.length; i++) {
+            for(int j = 0; j < reserve.length; j++) {
+                if(lost[i] == reserve[j]) {
+                    answer++;
+                    lost[i] = -1;
+                    reserve[j] = -1;
+                    break;
                 }
             }
         }
         
-        // 체육복을 가진 학생 수 세기
-        int answer = 0;
-        for (int c : clothes) {
-            if (c > 0) {
-                answer++;
+        for(int i = 0; i < lost.length; i++) {
+            for(int j = 0; j < reserve.length; j++) {
+                if(lost[i] - 1 == reserve[j] || lost[i] + 1 == reserve[j]) {
+                    answer++;
+                    reserve[j] = -1;
+                    break;
+                }
             }
         }
         
