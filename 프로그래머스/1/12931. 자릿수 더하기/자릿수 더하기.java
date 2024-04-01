19de09a943f4e20fa9f81969c62f936e963652ca
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-04-01 13:36:23 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.14 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -2,13 +2,14 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
+        String temp = String.valueOf(n);
+        String[] input = temp.split("");
         int answer = 0;
         
-        String[] input = String.valueOf(n).split("");
         for(int i = 0; i < input.length; i++) {
             answer += Integer.parseInt(input[i]);
         }
-        
+
         return answer;
     }
 }
