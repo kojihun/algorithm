3c0554e073c27c9a780aba75ca186b880e0e54a4
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-04-03 11:36:42 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.04 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -1,19 +1,23 @@
-public class Solution {
-    public static int solution(int n) {
-        // 주어진 숫자의 이진 표현에서 1의 개수를 카운트
-        int countOne = Integer.bitCount(n);
+class Solution {
+    public int solution(int n) {
+        int oneCount = countOnes(n);
+        for (int i = n + 1; ; i++) {
+            if (countOnes(i) == oneCount) {
+                return i;
+            }
+        }
+    }
+    
+    private int countOnes(int num) {
+        int count = 0;
+        String binaryString = Integer.toBinaryString(num);
         
-        // 다음 큰 숫자를 찾을 때까지 1씩 증가하며 조건을 만족하는 숫자 찾기
-        int nextNumber = n + 1;
-        while (Integer.bitCount(nextNumber) != countOne) {
-            nextNumber++;
+        for (char c : binaryString.toCharArray()) {
+            if (c == '1') {
+                count++;
+            }
         }
         
-        return nextNumber;
-    }
-
-    public static void main(String[] args) {
-        int n = 78;
-        System.out.println(solution(n)); // 다음 큰 숫자인 83 출력
+        return count;
     }
 }
