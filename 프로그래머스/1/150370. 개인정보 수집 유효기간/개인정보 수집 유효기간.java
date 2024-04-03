2e79c0bf3bc46bf405237e2f8da5b120b3738d6f
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-04-03 10:36:08 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 3.16 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -50,28 +50,11 @@ class Solution {
         
         if (day == 1) {
             month--;
-            if (month == 0) {
-                month = 12;
-                year--;
-            }
-            day = daysInMonth(year, month);
+            day = 28;
         } else {
             day--;
         }
         
         return new int[]{year, month, day};
-    }
-    
-    private int daysInMonth(int year, int month) {
-        switch (month) {
-            case 1: case 3: case 5: case 7: case 8: case 10: case 12:
-                return 31;
-            case 4: case 6: case 9: case 11:
-                return 30;
-            case 2:
-                return (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) ? 29 : 28;
-            default:
-                return -1;
-        }
     }
 }
