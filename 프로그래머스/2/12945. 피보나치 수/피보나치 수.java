8381230c9429ffa439219877f5f05550d0f905c2
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-04-04 10:53:49 +0900

    [level 2] Title: 피보나치 수, Time: 2.43 ms, Memory: 73.2 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,23 +1,13 @@
 class Solution {
-    public long solution(int n) {
-        // 1. 피보나치 수를 저장할 배열 선언
-        long[] fibonacci = new long[100001];
+    public int solution(int n) {
+        int[] array = new int[n + 1];
+        array[0] = 0;
+        array[1] = 1;
         
-        // 2. 0과 1은 초기화 필요
-        fibonacci[0] = 0;
-        fibonacci[1] = 1;
-        
-        // 3. 반복문을 순회하며 n지점까지의 값을 찾음
-        for(int i = 2; i < fibonacci.length; i++) {
-            fibonacci[i] = (fibonacci[i - 2] + fibonacci[i - 1])  % 1234567;
-            
-            // 4. i가 n이 되는 시점에 break;
-            if(i == n) {
-                break;
-            }
+        for(int i = 2; i <= n; i++) {
+            array[i] = (array[i - 2] + array[i - 1]) % 1234567;
         }
         
-        // 5. fibonacci의 n번째 값을 리턴
-        return fibonacci[n];
+        return array[n]; 
     }
 }
