246760db9481656d2d0f7d56786385914df91a5b
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-04-04 11:17:12 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.07 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,48 +1,30 @@
-import java.util.Map;
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
     public int[] solution(int n, String[] words) {
-        // 1. 단어와 말한 횟수를 저장하기 위해 Map 선언
-        Map<String, Integer> map = new HashMap<>();
         int[] answer = new int[2];
+        Set<String> set = new HashSet<>();
+        set.add(words[0]);
         
-        // 2. 최초 단어 추가
-        String word = words[0];
-        map.put(word, map.getOrDefault(word, 0) + 1);
-        
-        // 3. 배열을 순회한다.
-        int round = 1;
-        for(int i = 1; i < words.length; i++) {
-            // 라운드 증가
-            if(i % n == 0) {
-                round++;
+        for(int i = 0; i < words.length - 1; i++) {
+            char lastChar = words[i].charAt(words[i].length() - 1);
+            char firstChar = words[i + 1].charAt(0);
+            
+            if(!set.contains(words[i + 1])) {
+                set.add(words[i + 1]);
+            }else {
+                answer[0] = (i + 2) % n == 0 ? n : (i + 2) % n;
+                answer[1] = (i + 2) % n == 0 ? (i + 2) / n : (i + 2) / n + 1;
+                return answer;
             }
             
-            // 4. map에 현재 단어가 몇번 나왔는지 저장한다.
-            String beforeWord = words[i - 1];
-            String lastAlphabet = beforeWord.substring(beforeWord.length() - 1);
-            
-            word = words[i];
-            String firstAlphabet = word.substring(0, 1);
-            
-            // 5. 끝말잇기가 되는지 먼저 확인
-            if(!lastAlphabet.equals(firstAlphabet)) {
-                answer[0] = (i + 1) % n == 0 ? n : (i + 1) % n;
-                answer[1] = round;
-                break;
-            }
-            
-            // 6. 개수 추가
-            map.put(word, map.getOrDefault(word, 0) + 1);
-            
-            // 7. 2개 이상일 경우 값 세팅
-            if(map.get(word) > 1) {
-                answer[0] = (i + 1) % n == 0 ? n : (i + 1) % n;
-                answer[1] = round;
-                break;
+            if(lastChar != firstChar) {
+                answer[0] = (i + 2) % n == 0 ? n : (i + 2) % n;
+                answer[1] = (i + 2) % n == 0 ? (i + 2) / n : (i + 2) / n + 1;
+                return answer;
             }
         }
+        
         return answer;
     }
 }
