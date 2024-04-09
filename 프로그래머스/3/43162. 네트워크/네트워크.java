d5a53ef742f74e9f93e23d7fa32b5b5e83d1c8ad
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-04-09 11:38:31 +0900

    [level 3] Title: 네트워크, Time: 0.08 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,24 +1,24 @@
 class Solution {
     public int solution(int n, int[][] computers) {
-        boolean[] visited = new boolean[n];
         int answer = 0;
+        boolean[] visited = new boolean[computers.length];
         
-        for (int i = 0; i < n; i++) {
-            if (!visited[i]) {
-                dfs(computers, visited, i);
+        for(int i = 0; i < computers.length; i++){
+            if(visited[i] == false){
                 answer++;
-            }
+                DFS(i, visited, computers);
+            }            
         }
         
         return answer;
     }
     
-    private void dfs(int[][] computers, boolean[] visited, int start) {
-        visited[start] = true;
+    public void DFS(int node, boolean[] visited, int[][] computers){
+        visited[node] = true;
         
-        for (int i = 0; i < computers.length; i++) {
-            if (computers[start][i] == 1 && !visited[i]) {
-                dfs(computers, visited, i);
+        for(int i = 0; i < computers.length; i++){
+            if(visited[i] == false && computers[node][i] == 1){
+                DFS(i, visited, computers);
             }
         }
     }
