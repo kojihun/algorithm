59e2034db5bc875e2af7f2219371d26dddfa7fd6
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-04-26 15:38:41 +0900

    [level 2] Title: 프로세스, Time: 0.73 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,31 +2,25 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        int answer = 0; // 출력 순서를 나타내는 변수
-        
-        // 우선순위를 저장하는 우선순위 큐 생성 (내림차순으로 정렬)
-        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
-        for(int priority : priorities) {
-            priorityQueue.offer(priority); // 우선순위 큐에 각 작업의 우선순위 추가
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        for(int i = 0; i < priorities.length; i++) {
+            queue.offer(priorities[i]);
         }
         
-        // 우선순위 큐가 비어있지 않은 동안 반복
-        while(!priorityQueue.isEmpty()) {
-            // 모든 작업에 대해 순회
+        int answer = 0;
+        while(!queue.isEmpty()) {
             for(int i = 0; i < priorities.length; i++) {
-                // 우선순위 큐에서 현재 가장 높은 우선순위를 가진 작업과 배열의 작업 비교
-                if(priorityQueue.peek() == priorities[i]) {
-                    priorityQueue.poll(); // 가장 높은 우선순위를 가진 작업 삭제
-                    answer++; // 출력 순서 증가
+                if(queue.peek() == priorities[i]) {
+                    queue.poll();
+                    answer++;
                     
-                    // 현재 작업이 원하는 위치인지 확인
                     if(location == i) {
-                        return answer; // 위치와 일치하면 출력 순서 반환 후 종료
+                        return answer;
                     }
                 }
             }
         }
         
-        return answer; // 출력 순서 반환
+        return answer;
     }
 }
