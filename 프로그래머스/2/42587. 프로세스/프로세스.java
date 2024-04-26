60b4047d0a3e5838b7244eb54645cf3535fbdaa0
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-04-26 15:22:05 +0900

    [level 2] Title: 프로세스, Time: 0.60 ms, Memory: 80.8 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -2,48 +2,31 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] priorities, int location) {
-        // 1. Map<Integer, Integer>의 형태로 위치까지 저장하면 어떨까? -> 두 개의 큐가 필요
-        Queue<Map<Integer, Integer>> beforeQueue = new LinkedList<>();
-        Queue<Map<Integer, Integer>> afterQueue = new LinkedList<>();
+        int answer = 0; // 출력 순서를 나타내는 변수
         
-        // 2. 반복문을 순회하며 현재의 값을 큐에 넣자!
-        for(int i = 0; i < priorities.length; i++) {
-            // 현재 원소와 위치 정보를 저장하자.
-            beforeQueue.offer(new HashMap<>(Map.of(priorities[i], i)));
+        // 우선순위를 저장하는 우선순위 큐 생성 (내림차순으로 정렬)
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<>(Collections.reverseOrder());
+        for(int priority : priorities) {
+            priorityQueue.offer(priority); // 우선순위 큐에 각 작업의 우선순위 추가
         }
         
-        // 3. priorities 배열을 정렬하자.
-        int index = priorities.length - 1;
-        Arrays.sort(priorities);
-        
-        // 4. beforeQueue가 비어있을 때까지 반복 순회하자.
-        while(!beforeQueue.isEmpty()) {
-            Map<Integer, Integer> currentMap = beforeQueue.poll();
-            int currentKey = currentMap.keySet().iterator().next();
-            
-            // 현재 키와 최댓값이 같다면
-            if(currentKey == priorities[index]) {
-                afterQueue.offer(currentMap);
-                index--;
-            }
-            // 다를 경우 다시 Queue에 넣는다.
-            else {
-                beforeQueue.offer(currentMap);
+        // 우선순위 큐가 비어있지 않은 동안 반복
+        while(!priorityQueue.isEmpty()) {
+            // 모든 작업에 대해 순회
+            for(int i = 0; i < priorities.length; i++) {
+                // 우선순위 큐에서 현재 가장 높은 우선순위를 가진 작업과 배열의 작업 비교
+                if(priorityQueue.peek() == priorities[i]) {
+                    priorityQueue.poll(); // 가장 높은 우선순위를 가진 작업 삭제
+                    answer++; // 출력 순서 증가
+                    
+                    // 현재 작업이 원하는 위치인지 확인
+                    if(location == i) {
+                        return answer; // 위치와 일치하면 출력 순서 반환 후 종료
+                    }
+                }
             }
         }
         
-        // 5. afterQueue를 반복 순회하며 결과를 찾는다.
-        int answer = -1;
-        for(int i = 0; i < afterQueue.size(); i++) {
-            Map<Integer, Integer> map = afterQueue.poll();
-            int currentValue = map.entrySet().iterator().next().getValue();
-            if(location == currentValue) {
-                answer = i + 1;
-            }
-            afterQueue.offer(map);
-        }
-
-        return answer;
-        
+        return answer; // 출력 순서 반환
     }
 }
