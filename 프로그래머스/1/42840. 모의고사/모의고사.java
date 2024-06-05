1a7d9c46fdc068e54a9be9b08b33c3530ce651f5
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-06-05 12:51:29 +0900

    [level 1] Title: 모의고사, Time: 2.54 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,39 +1,44 @@
 import java.util.*;
 
-/*
-1. 1, 2, 3, 4, 5
-2. 2, 1, 2, 3, 2, 4, 2, 5
-3. 3, 3, 1, 1, 2, 2, 4, 4, 5, 5
-*/
 class Solution {
     public int[] solution(int[] answers) {
-        int[] player1 = {1, 2, 3, 4, 5};
-        int[] player2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] player3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int[] scores = new int[3];
+        int[] persons = new int[3];
+        int[] person1 = {1, 2, 3, 4, 5};
+        int[] person2 = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] person3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
+        int maxCount = Integer.MIN_VALUE;
         for(int i = 0; i < answers.length; i++) {
-            if(answers[i] == player1[i % player1.length]) {
-                scores[0]++;
+            int answer = answers[i];
+            if(answer == person1[i % person1.length]) {
+                persons[0]++;
+                if(maxCount < persons[0]) {
+                    maxCount = persons[0];
+                }
             }
             
-            if(answers[i] == player2[i % player2.length]) {
-                scores[1]++;
+            if(answer == person2[i % person2.length]) {
+                persons[1]++;
+                if(maxCount < persons[1]) {
+                    maxCount = persons[1];
+                }
             }
             
-            if(answers[i] == player3[i % player3.length]) {
-                scores[2]++;
+            if(answer == person3[i % person3.length]) {
+                persons[2]++;
+                if(maxCount < persons[2]) {
+                    maxCount = persons[2];
+                }
             }
         }
         
-        int maxValue = Math.max(scores[0], Math.max(scores[1], scores[2]));
-        List<Integer> answer = new ArrayList<>();
-        for(int i = 0; i < scores.length; i++) {
-            if(scores[i] == maxValue) {
-                answer.add(i + 1);
+        List<Integer> list = new ArrayList<>();
+        for(int i = 0; i < persons.length; i++) {
+            if(persons[i] == maxCount) {
+                list.add(i + 1);
             }
         }
         
-        return answer.stream().mapToInt(Integer::intValue).toArray();
+        return list.stream().mapToInt(i -> i).toArray();
     }
 }
