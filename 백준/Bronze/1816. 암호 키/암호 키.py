272080c7850bfd2bac95d6b1ba6574bb8395dbc6
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-12-02 23:19:57 +0900

    [Bronze I] Title: 암호 키, Time: 1188 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/1816. 암호 키/암호 키.py
@@ -0,0 +1,13 @@
+# 테스트 케이스 TC
+TC = int(input())
+
+for _ in range(TC):
+    number = int(input())
+
+    for i in range(2, 1_000_001):
+        if number % i == 0:
+            print("NO")
+            break
+        if i == 1_000_000:
+            print("YES")
+            break
