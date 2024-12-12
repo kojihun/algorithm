4e98add235cab088cd30975b79dea4792578be4b
NEWTOWNBOY <38236367+kojihun@users.noreply.github.com>
2024-12-12 23:04:02 +0900

    [Silver IV] Title: 수 찾기, Time: 148 ms, Memory: 52272 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,41 +1,15 @@
-def binary_search(numbers, find_number):
-    left = 0
-    right = len(numbers) - 1
-
-    while left <= right:
-        mid = (left + right) // 2
-
-        if numbers[mid] < find_number:
-            left = mid + 1
-
-        if numbers[mid] > find_number:
-            right = mid - 1
-
-        if numbers[mid] == find_number:
-            return 1
-
-    return 0
-
-
 # N
 N = int(input())
 
-n_numbers = list(map(int, input().split()))
+n_numbers = set(map(int, input().split()))  # 리스트 대신 집합 사용
 
 # M
 M = int(input())
 
 m_numbers = list(map(int, input().split()))
 
-# 이분 탐색을 위한 정렬
-n_numbers.sort()
-
-# 결과를 담을 answer
-answer = []
-
 for number in m_numbers:
-    answer.append(binary_search(n_numbers, number))
-
-for is_exist in answer:
-    print(is_exist)
-
+    if number in n_numbers:
+        print(1)
+    else:
+        print(0)
